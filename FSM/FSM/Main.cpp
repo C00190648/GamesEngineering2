44355fbc7d4c7b1ce592e815a5596cb99e91154c
C00190648 <c00190648@itcarlow.ie>
2017-10-11 16:15:36 +0100

    some animation

--- FSM/FSM/Main.cpp
+++ FSM/FSM/Main.cpp
@@ -1,54 +1,61 @@
 #include <SDL.h>
+#include <SDL_image.h>
+#include<iostream>
 #include "Animation.h"
 
 const int WIDTH = 800, HEIGHT = 600;
+const int FRAMES = 10;
+
+
 
 int main(int argc, char *argv[])
 {
-	SDL_Init(SDL_INIT_EVERYTHING);
-
-	Animation fsm;
+	bool quit = false;
 	SDL_Event event;
-	SDL_Surface *windowSurface = NULL;
 
-	SDL_Window *window = SDL_CreateWindow("Hello SDL World", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WIDTH, HEIGHT, SDL_WINDOW_ALLOW_HIGHDPI);
+	SDL_Init(SDL_INIT_VIDEO);
+	IMG_Init(IMG_INIT_PNG);
 
-	windowSurface = SDL_GetWindowSurface(window);
+	SDL_Window * window = SDL_CreateWindow("SDL2 Sprite Sheets",
+		SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WIDTH,
+		HEIGHT, 0);
+	SDL_Renderer * renderer = SDL_CreateRenderer(window, -1, 0);
+	SDL_Surface * image = IMG_Load("playerSpriteSheetBlue.png");
+	SDL_Texture * texture = SDL_CreateTextureFromSurface(renderer,
+		image);
 
-	while (true)
+
+	SDL_SetRenderDrawColor(renderer, 168, 230, 255, 255);
+	SDL_RenderClear(renderer);
+
+	while (!quit)
 	{
-		if (SDL_PollEvent(&event))
+		Uint32 ticks = SDL_GetTicks();
+		Uint32 sprite = (ticks / 100) % 8;
+		
+
+		SDL_Rect srcrect = { sprite * 260.1, 510, 260, 260 };
+		SDL_Rect dstrect = { 10, 10, 265, 260 };
+
+		SDL_PollEvent(&event);
+
+		switch (event.type)
 		{
-			if (SDL_QUIT == event.type)
-			{
-				break;
-			}
-
-			switch (event.type)
-			{
-			case SDL_KEYUP:
-				switch (event.key.keysym.sym)
-				{
-				case SDLK_SPACE:
-					fsm.jumping();
-				}
-			case SDL_KEYDOWN:
-				switch (event.key.keysym.sym)
-				{
-				case SDLK_LEFT:
-					fsm.moveLeft();
-
-				case SDLK_RIGHT:
-					fsm.moveRight();
-
-				}
-			}
-
-
+		case SDL_QUIT:
+			quit = true;
+			break;
 		}
-		SDL_UpdateWindowSurface(window);
+		SDL_RenderClear(renderer);
+		SDL_RenderCopy(renderer, texture, &srcrect, &dstrect);
+		SDL_RenderPresent(renderer);
 	}
-	SDL_FreeSurface(windowSurface);
-	system("pause");
+
+	SDL_DestroyTexture(texture);
+	SDL_FreeSurface(image);
+	SDL_DestroyRenderer(renderer);
+	SDL_DestroyWindow(window);
+	IMG_Quit();
+	SDL_Quit();
+
 	return 0;
 }
